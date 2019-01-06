a69c7b6052850ab29005d5af81fb9fc6079a3888
Craig Miller <craigmiller160@gmail.com>
2019-01-06 15:34:41 -0500

    Added the router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import store from 'store/store';
 
 class App extends Component {
@@ -7,9 +8,11 @@ class App extends Component {
     render() {
         return (
             <Provider store={store}>
-                <div>
-                    <h1>My App</h1>
-                </div>
+                <BrowserRouter>
+                    <div>
+                        <h1>My App</h1>
+                    </div>
+                </BrowserRouter>
             </Provider>
         );
     }
