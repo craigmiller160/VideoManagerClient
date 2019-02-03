45cd79eae766817fbb7da2ccf879d7009b006083
Craig Miller <craigmiller160@gmail.com>
2019-02-03 09:15:36 -0500

    Created unit tests for VideoListItem

--- src/components/AppContent/VideoListLayout/VideoList/VideoListItem/__tests__/VideoListItem.Spec.js
+++ src/components/AppContent/VideoListLayout/VideoList/VideoListItem/__tests__/VideoListItem.Spec.js
@@ -2,12 +2,40 @@ import React from 'react';
 import { mount } from 'enzyme';
 import VideoListItem from '../VideoListItem';
 
+const expandVideoFile = jest.fn();
+const props = {
+    videoFile: {
+        fileId: 1,
+        expanded: false,
+        series: [],
+        categories: [],
+        stars: []
+    },
+    expandVideoFile
+};
+
+const createComponent = (props = {}) => mount(
+    <VideoListItem { ...props } />
+);
+
 describe('VideoListItem', () => {
     it('shows smaller view', () => {
-        throw new Error('Finish this');
+        const component = createComponent(props);
+        expect(component.find('Collapse').props()).toEqual(expect.objectContaining({
+            isOpen: false
+        }));
     });
 
     it('shows expanded view', () => {
-        throw new Error('Finish this');
+        const component = createComponent({ ...props, videoFile: { ...props.videoFile, expanded: true } });
+        expect(component.find('Collapse').props()).toEqual(expect.objectContaining({
+            isOpen: true
+        }));
+    });
+
+    it('handles clicks', () => {
+        const component = createComponent(props);
+        component.simulate('click');
+        expect(expandVideoFile).toHaveBeenLastCalledWith(1);
     });
 });
