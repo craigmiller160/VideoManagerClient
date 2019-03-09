8706319dcdf07e0a1b3452350f1962edc0fb1d27
Craig Miller <craigmiller160@gmail.com>
2019-03-09 16:08:14 -0500

    Refactored VideoSearch into a functional component

--- src/components/AppContent/VideoListLayout/VideoSearch/VideoSearch.js
+++ src/components/AppContent/VideoListLayout/VideoSearch/VideoSearch.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React  from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { reset } from 'redux-form';
@@ -11,78 +11,75 @@ import { searchForVideos } from 'store/videoList/videoList.actions';
 
 export const FORM_NAME = 'video-search';
 
-export class VideoSearch extends Component {
+const VideoSearch = (props) => {
+    const {
+        filters: { categories, series, stars },
+        searchForVideos,
+        resetForm
+    } = props;
 
-    resetForm = () => {
-        this.props.resetForm(FORM_NAME);
-        this.props.searchForVideos();
+    const doResetForm = () => {
+        resetForm(FORM_NAME);
+        searchForVideos();
     };
 
-    render() {
-        const {
-            categories,
-            series,
-            stars
-        } = this.props.filters;
-
-        return (
-            <Form
-                form={ FORM_NAME }
-                handleSubmit={ this.props.searchForVideos }
-                className={ classes.VideoSearch }
-            >
-                <Row>
-                    <Col sm="6" md="3">
-                        <Input
-                            label="Search"
-                            name="search"
-                        />
-                    </Col>
-                    <Col sm="6" md="3">
-                        <Select
-                            label="Category"
-                            options={ categories }
-                            name="category"
-                        />
-                    </Col>
-                    <Col sm="6" md="3">
-                        <Select
-                            label="Series"
-                            options={ series }
-                            name="series"
-                        />
-                    </Col>
-                    <Col sm="6" md="3">
-                        <Select
-                            label="Stars"
-                            options={ stars }
-                            name="star"
-                        />
-                    </Col>
-                </Row>
-                <Row>
-                    <Col className="text-center">
-                        <Button
-                            id="do-video-search-btn"
-                            type="submit"
-                            color="primary"
-                        >
-                            Search
-                        </Button>
-                        <Button
-                            id="reset-video-search-btn"
-                            type="button"
-                            color="info"
-                            onClick={ this.resetForm }
-                        >
-                            Reset
-                        </Button>
-                    </Col>
-                </Row>
-            </Form>
-        );
-    }
-}
+    return (
+        <Form
+            form={ FORM_NAME }
+            handleSubmit={ searchForVideos }
+            className={ classes.VideoSearch }
+        >
+            <Row>
+                <Col sm="6" md="3">
+                    <Input
+                        label="Search"
+                        name="search"
+                    />
+                </Col>
+                <Col sm="6" md="3">
+                    <Select
+                        label="Category"
+                        options={ categories }
+                        name="category"
+                    />
+                </Col>
+                <Col sm="6" md="3">
+                    <Select
+                        label="Series"
+                        options={ series }
+                        name="series"
+                    />
+                </Col>
+                <Col sm="6" md="3">
+                    <Select
+                        label="Stars"
+                        options={ stars }
+                        name="star"
+                    />
+                </Col>
+            </Row>
+            <Row>
+                <Col className="text-center">
+                    <Button
+                        id="do-video-search-btn"
+                        type="submit"
+                        color="primary"
+                    >
+                        Search
+                    </Button>
+                    <Button
+                        id="reset-video-search-btn"
+                        type="button"
+                        color="info"
+                        onClick={ doResetForm }
+                    >
+                        Reset
+                    </Button>
+                </Col>
+            </Row>
+        </Form>
+    );
+};
 
 const mapStateToProps = (state) => ({
     filters: state.videoSearch.filters
