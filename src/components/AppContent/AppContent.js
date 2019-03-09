474aca09cec14a96edb16d4f22f99b39fdce5b10
Craig Miller <craigmiller160@gmail.com>
2019-03-09 16:25:57 -0500

    Refactored AppContent to be a functionaly component

--- src/components/AppContent/AppContent.js
+++ src/components/AppContent/AppContent.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import VideoNavbar from 'components/AppContent/VideoNavbar/VideoNavbar';
 import VideoListLayout from './VideoListLayout/VideoListLayout';
 import { Container, Row, Col } from 'reactstrap';
@@ -16,120 +16,140 @@ import classes from './AppContent.scss';
 import { loadFilterOptions } from 'store/videoSearch/videoSearch.actions';
 import { saveVideoFileEdits } from 'store/videoList/videoList.actions';
 
-export class AppContent extends Component {
+const resetToRootComponent = (props) => {
+    const {
+        history: { location },
+        isScanning,
+        selectedVideo
+    } = props;
 
-    state = {
-        isStarted: false
-    };
-
-    componentDidMount = async () => {
-        try {
-            this.props.loadFilterOptions();
-            await this.props.checkIsScanning();
-            this.setState({ isStarted: true });
-        }
-        catch (ex) {
-            this.props.showErrorAlert(ex.message);
-        }
-    };
-
-    saveFileChanges = async () => {
-        await this.props.saveVideoFileEdits();
-        this.props.history.push('/');
-    };
-
-    static resetToRootComponent(nextProps) {
-        if (nextProps.history.location.pathname === '/scanning' && !nextProps.isScanning) {
-            return true;
-        }
-
-        return nextProps.history.location.pathname === '/edit' && (!nextProps.selectedVideo || Object.entries(nextProps.selectedVideo).length === 0);
+    if (location.pathname === '/scanning' && !isScanning) {
+        return true;
     }
 
-    componentWillUpdate(nextProps) {
-        if (AppContent.resetToRootComponent(nextProps)) {
-            this.props.history.push('/');
-        }
-        else if (nextProps.isScanning && nextProps.history.location.pathname !== '/scanning') {
-            nextProps.history.push('/scanning');
-        }
-    }
+    return location.pathname === '/edit' && (!selectedVideo || Object.entries(selectedVideo).length === 0);
+};
 
-    render() {
-        const {
-            startFileScan,
-            isScanning,
-            checkIsScanning,
-            alert,
-            hideAlert,
-            selectedVideo,
-            filters
-        } = this.props;
-        const { isStarted } = this.state;
+const handleRouting = (props) => {
+    const {
+        history,
+        isScanning
+    } = props;
 
-        return (
-            <div className={ classes.AppContent }>
-                <VideoNavbar
-                    startFileScan={ startFileScan }
-                    isScanning={ isScanning }
-                />
-                {
-                    isStarted &&
-                        <Container className={ classes.container }>
-                            <Row>
-                                <Col xs={{ size: 8, offset: 2 }}>
-                                    <Alert
-                                        alert={ alert }
-                                        hideAlert={ hideAlert }
-                                    />
-                                </Col>
-                            </Row>
-                            <Switch>
-                                <Route
-                                    path="/scanning"
-                                    render={ (props) => (
-                                        <Scanning
-                                            { ...props }
-                                            isScanning={ isScanning }
-                                            checkIsScanning={ checkIsScanning }
-                                        />
-                                    ) }
-                                />
-                                <Route
-                                    path="/edit"
-                                    render={ (props) => (
-                                        <VideoFileEdit
-                                            { ...props }
-                                            selectedVideo={ selectedVideo }
-                                            saveFileChanges={ this.saveFileChanges }
-                                            filters={ filters }
-                                        />
-                                    ) }
-                                />
-                                <Route
-                                    path="/filters"
-                                    render={ (props) => (
-                                        <ManageVideoFilters
-                                            { ...props }
-                                        />
-                                    ) }
-                                />
-                                <Route path="/"
-                                       exact
-                                       render={ (props) => (
-                                           <VideoListLayout
-                                               { ...props }
-                                               isScanning={ isScanning }
-                                           />
-                                       ) }
-                                />
-                            </Switch>
-                        </Container>
-                }
-            </div>
-        );
+    if (resetToRootComponent(props)) {
+        history.push('/');
     }
-}
+    else if (isScanning && history.location.pathname !== '/scanning') {
+        history.push('/scanning');
+    }
+};
+
+const startupCheck = async (props, setStarted) => {
+    const {
+        loadFilterOptions,
+        checkIsScanning,
+        showErrorAlert
+    } = props;
+    try {
+        loadFilterOptions();
+        await checkIsScanning();
+        setStarted(true);
+    }
+    catch (ex) {
+        showErrorAlert(ex.message);
+    }
+};
+
+const AppContent = (props) => {
+    const [ isStarted, setStarted ] = useState(false);
+    const {
+        checkIsScanning,
+        saveVideoFileEdits,
+        history,
+        isScanning,
+        selectedVideo,
+        startFileScan,
+        alert,
+        hideAlert,
+        filters
+    } = props;
+
+    useEffect(() => {
+        startupCheck(props, setStarted);
+    }, []);
+
+    useEffect(() => {
+        handleRouting(props);
+        return () => handleRouting(props);
+    }, [history, isScanning, selectedVideo]);
+
+    const saveFileChanges = async () => {
+        await saveVideoFileEdits();
+        history.push('/');
+    };
+
+    return (
+        <div className={ classes.AppContent }>
+            <VideoNavbar
+                startFileScan={ startFileScan }
+                isScanning={ isScanning }
+            />
+            {
+                isStarted &&
+                <Container className={ classes.container }>
+                    <Row>
+                        <Col xs={{ size: 8, offset: 2 }}>
+                            <Alert
+                                alert={ alert }
+                                hideAlert={ hideAlert }
+                            />
+                        </Col>
+                    </Row>
+                    <Switch>
+                        <Route
+                            path="/scanning"
+                            render={ (props) => (
+                                <Scanning
+                                    { ...props }
+                                    isScanning={ isScanning }
+                                    checkIsScanning={ checkIsScanning }
+                                />
+                            ) }
+                        />
+                        <Route
+                            path="/edit"
+                            render={ (props) => (
+                                <VideoFileEdit
+                                    { ...props }
+                                    selectedVideo={ selectedVideo }
+                                    saveFileChanges={ saveFileChanges }
+                                    filters={ filters }
+                                />
+                            ) }
+                        />
+                        <Route
+                            path="/filters"
+                            render={ (props) => (
+                                <ManageVideoFilters
+                                    { ...props }
+                                />
+                            ) }
+                        />
+                        <Route path="/"
+                               exact
+                               render={ (props) => (
+                                   <VideoListLayout
+                                       { ...props }
+                                       isScanning={ isScanning }
+                                   />
+                               ) }
+                        />
+                    </Switch>
+                </Container>
+            }
+        </div>
+    );
+};
 
 const mapStateToProps = (state) => ({
     isScanning: state.scanning.isScanning,
