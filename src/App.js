f0051fda331086c4ae62388463e35b379da1957a
Craig Miller <craigmiller160@gmail.com>
2019-03-09 16:05:35 -0500

    Refactored App and VideoList components to be functional

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import store from './store/store';
 import AppContent from './components/AppContent/AppContent';
 import './App.scss';
 
-class App extends Component {
-
-    render() {
-        return (
-            <Provider store={ store }>
-                <BrowserRouter>
-                    <AppContent />
-                </BrowserRouter>
-            </Provider>
-        );
-    }
-}
+const App = () => (
+    <Provider store={ store }>
+        <BrowserRouter>
+            <AppContent />
+        </BrowserRouter>
+    </Provider>
+);
 
 export default App;
--- src/components/AppContent/VideoListLayout/VideoList/VideoList.js
+++ src/components/AppContent/VideoListLayout/VideoList/VideoList.js
@@ -1,98 +1,96 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import classes from './VideoList.scss';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import VideoListItem from './VideoListItem/VideoListItem';
 import { ListGroup } from 'reactstrap';
-import { expandVideoFile, searchForVideos, setCurrentPage } from '../../../../store/videoList/videoList.actions';
+import { expandVideoFile, searchForVideos, setCurrentPage } from 'store/videoList/videoList.actions';
 import Spinner from 'components/UI/Spinner/Spinner';
 import Pagination, { RIGHT_ALIGN } from '../../../UI/Pagination/Pagination';
 
-export class VideoList extends Component {
-
-    componentDidMount() {
-        this.props.searchForVideos();
+const getPagination = (props) => {
+    const { totalItems, itemsPerPage, currentPage, setCurrentPage } = props;
+    if (totalItems === 0) {
+        return <div />;
     }
 
-    getPagination() {
-        const { totalItems, itemsPerPage, currentPage } = this.props;
-        if (totalItems === 0) {
-            return <div />;
+    const totalPages = Math.ceil(totalItems / itemsPerPage);
+
+    return (
+        <Pagination
+            totalPages={ totalPages }
+            currentPage={ currentPage }
+            align={ RIGHT_ALIGN }
+            onClick={ (value) => paginationClick(value, currentPage, setCurrentPage) }
+        />
+    );
+};
+
+const paginationClick = (value, currentPage, setCurrentPage) => {
+    let newPage = currentPage;
+    if (newPage != value) { // eslint-disable-line
+        if ('<' === value) {
+            newPage--;
+        }
+        else if ('>' === value) {
+            newPage++;
+        }
+        else {
+            newPage = parseInt(value);
         }
 
-        const totalPages = Math.ceil(totalItems / itemsPerPage);
-
-        return (
-            <Pagination
-                totalPages={ totalPages }
-                currentPage={ currentPage }
-                align={ RIGHT_ALIGN }
-                onClick={ this.paginationClick }
-            />
-       );
+        setCurrentPage(newPage);
+        searchForVideos();
     }
+};
 
-    paginationClick = (value) => {
-        let currentPage = this.props.currentPage;
-        if (currentPage != value) { // eslint-disable-line
-            if ('<' === value) {
-                currentPage--;
-            }
-            else if ('>' === value) {
-                currentPage++;
-            }
-            else {
-                currentPage = parseInt(value);
-            }
+const VideoList = (props) => {
+    const {
+        searching,
+        videoList,
+        expandVideoFile,
+        searchForVideos,
+        currentPage
+    } = props;
 
-            this.props.setCurrentPage(currentPage);
-            this.props.searchForVideos();
-        }
-    };
+    useEffect(() => {
+        searchForVideos();
+    }, [currentPage]);
 
-    render() {
-        const {
-            searching,
-            videoList,
-            expandVideoFile
-        } = this.props;
-
-        return (
-            <div className={ classes.VideoList }>
-                <div className={ classes['list-title'] }>
-                    <h3>Available Videos</h3>
-                </div>
-                {
-                    searching &&
-                        <Spinner />
-                }
-                {
-                    !searching && videoList.length > 0 &&
-                        <div>
-                            { this.getPagination() }
-                            <ListGroup>
-                                { videoList.map((videoFile) => (
-                                    <VideoListItem
-                                        key={ videoFile.fileId }
-                                        videoFile={ videoFile }
-                                        expandVideoFile={ expandVideoFile }
-                                    />
-                                )) }
-                            </ListGroup>
-                            { this.getPagination() }
-                        </div>
-                }
-                {
-                    !searching && videoList.length === 0 &&
-                        <div className={ classes['none-available'] }>
-                            <h3>No Videos Available</h3>
-                        </div>
-                }
+    return (
+        <div className={ classes.VideoList }>
+            <div className={ classes['list-title'] }>
+                <h3>Available Videos</h3>
             </div>
-        );
-    }
-
-}
+            {
+                searching &&
+                <Spinner />
+            }
+            {
+                !searching && videoList.length > 0 &&
+                <div>
+                    { getPagination(props) }
+                    <ListGroup>
+                        { videoList.map((videoFile) => (
+                            <VideoListItem
+                                key={ videoFile.fileId }
+                                videoFile={ videoFile }
+                                expandVideoFile={ expandVideoFile }
+                            />
+                        )) }
+                    </ListGroup>
+                    { getPagination(props) }
+                </div>
+            }
+            {
+                !searching && videoList.length === 0 &&
+                <div className={ classes['none-available'] }>
+                    <h3>No Videos Available</h3>
+                </div>
+            }
+        </div>
+    );
+};
 
 const mapStateToProps = (state) => ({
     totalItems: state.videoList.pagination.totalItems,
