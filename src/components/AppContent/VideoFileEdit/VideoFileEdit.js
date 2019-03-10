66239affdd4fc264be1585c3e6b1f8cdc3c87d79
Craig Miller <craigmiller160@gmail.com>
2019-03-10 11:45:56 -0400

    Added prompt for unsaved changes

--- src/components/AppContent/VideoFileEdit/VideoFileEdit.js
+++ src/components/AppContent/VideoFileEdit/VideoFileEdit.js
@@ -1,16 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import Form from 'components/UI/form/Form/Form';
 import classes from './VideoFileEdit.scss';
 import { Col, Row, Button } from 'reactstrap';
 import Input from 'components/UI/form/Input/Input';
 import Select from 'components/UI/form/Select/Select';
+import { Prompt } from 'react-router';
 
 export const FORM_NAME = 'video-file-edit';
 
-// TODO need some way to warn that unsaved changes will be lost when leaving this component
-
 const VideoFileEdit = (props) => {
+    const [ isSubmitted, setSubmitted ] = useState(false);
+
     if (!props.selectedVideo) {
         return <div />;
     }
@@ -22,75 +23,86 @@ const VideoFileEdit = (props) => {
         saveFileChanges
     } = props;
 
+    const submit = () => {
+        setSubmitted(true);
+        saveFileChanges();
+    };
+
     return (
-        <Form
-            form={ FORM_NAME }
-            handleSubmit={ saveFileChanges }
-            className={ classes.VideoFileEdit }
-            initialValues={ selectedVideo }
-        >
-            <Row>
-                <Col className="text-center">
-                    <h3 className={ classes.title }>{ fileName }</h3>
-                </Col>
-            </Row>
-            <Row>
-                <Col md={ { size: 4, offset: 4 } }>
-                    <Input
-                        label="Display Name"
-                        name="displayName"
-                    />
-                </Col>
-            </Row>
-            <Row>
-                <Col md="4">
-                    <Select
-                        label="Categories"
-                        options={ categories }
-                        name="categories"
-                        multi
-                    />
-                </Col>
-                <Col md="4">
-                    <Select
-                        label="Series"
-                        options={ series }
-                        name="series"
-                        multi
-                    />
-                </Col>
-                <Col md="4">
-                    <Select
-                        label="Stars"
-                        options={ stars }
-                        name="stars"
-                        multi
-                    />
-                </Col>
-            </Row>
-            <Row>
-                <Col md={ { size: 6, offset: 3 } }>
-                    <Input
-                        type="textarea"
-                        name="description"
-                        label="Description"
-                        textarea={ {
-                            rows: 5
-                        } }
-                    />
-                </Col>
-            </Row>
-            <Row>
-                <Col className="text-center">
-                    <Button
-                        color="success"
-                        type="submit"
-                    >
-                        Save
-                    </Button>
-                </Col>
-            </Row>
-        </Form>
+        <>
+            <Prompt
+                when={ !isSubmitted }
+                message="Are you sure you don't want to save your changes?"
+            />
+            <Form
+                form={ FORM_NAME }
+                handleSubmit={ submit }
+                className={ classes.VideoFileEdit }
+                initialValues={ selectedVideo }
+            >
+                <Row>
+                    <Col className="text-center">
+                        <h3 className={ classes.title }>{ fileName }</h3>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col md={ { size: 4, offset: 4 } }>
+                        <Input
+                            label="Display Name"
+                            name="displayName"
+                        />
+                    </Col>
+                </Row>
+                <Row>
+                    <Col md="4">
+                        <Select
+                            label="Categories"
+                            options={ categories }
+                            name="categories"
+                            multi
+                        />
+                    </Col>
+                    <Col md="4">
+                        <Select
+                            label="Series"
+                            options={ series }
+                            name="series"
+                            multi
+                        />
+                    </Col>
+                    <Col md="4">
+                        <Select
+                            label="Stars"
+                            options={ stars }
+                            name="stars"
+                            multi
+                        />
+                    </Col>
+                </Row>
+                <Row>
+                    <Col md={ { size: 6, offset: 3 } }>
+                        <Input
+                            type="textarea"
+                            name="description"
+                            label="Description"
+                            textarea={ {
+                                rows: 5
+                            } }
+                        />
+                    </Col>
+                </Row>
+                <Row>
+                    <Col className="text-center">
+                        <Button
+                            color="success"
+                            type="submit"
+                        >
+                            Save
+                        </Button>
+                    </Col>
+                </Row>
+            </Form>
+        </>
     );
 };
 
