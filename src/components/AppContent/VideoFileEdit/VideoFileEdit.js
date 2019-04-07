147de129958bed9c7ee74d64800cc81f057c9640
Craig Miller <craigmiller160@gmail.com>
2019-04-07 12:02:18 -0400

    Added the add button to the video file edit page

--- src/components/AppContent/VideoFileEdit/VideoFileEdit.js
+++ src/components/AppContent/VideoFileEdit/VideoFileEdit.js
@@ -40,67 +40,92 @@ const VideoFileEdit = (props) => {
                 className={ classes.VideoFileEdit }
                 initialValues={ selectedVideo }
             >
-                <Row>
-                    <Col className="text-center">
-                        <h3 className={ classes.title }>{ fileName }</h3>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col md={ { size: 4, offset: 4 } }>
-                        <Input
-                            label="Display Name"
-                            name="displayName"
-                        />
-                    </Col>
-                </Row>
-                <Row>
-                    <Col md="4">
-                        <Select
-                            label="Categories"
-                            options={ categories }
-                            name="categories"
-                            multi
-                        />
-                    </Col>
-                    <Col md="4">
-                        <Select
-                            label="Series"
-                            options={ series }
-                            name="series"
-                            multi
-                        />
-                    </Col>
-                    <Col md="4">
-                        <Select
-                            label="Stars"
-                            options={ stars }
-                            name="stars"
-                            multi
-                        />
-                    </Col>
-                </Row>
-                <Row>
-                    <Col md={ { size: 6, offset: 3 } }>
-                        <Input
-                            type="textarea"
-                            name="description"
-                            label="Description"
-                            textarea={ {
-                                rows: 5
-                            } }
-                        />
-                    </Col>
-                </Row>
-                <Row>
-                    <Col className="text-center">
-                        <Button
-                            color="success"
-                            type="submit"
-                        >
-                            Save
-                        </Button>
-                    </Col>
-                </Row>
+                <>
+                    <Row>
+                        <Col className="text-center">
+                            <h3 className={ classes.title }>{ fileName }</h3>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col md={ { size: 4, offset: 4 } }>
+                            <Input
+                                label="Display Name"
+                                name="displayName"
+                            />
+                        </Col>
+                    </Row>
+                    <Row className="align-items-center">
+                        <Col md="4">
+                            <Select
+                                label="Categories"
+                                options={ categories }
+                                name="categories"
+                                multi
+                            />
+                        </Col>
+                        <Col md="4">
+                            <Select
+                                label="Series"
+                                options={ series }
+                                name="series"
+                                multi
+                            />
+                        </Col>
+                        <Col md="4">
+                            <Select
+                                label="Stars"
+                                options={ stars }
+                                name="stars"
+                                multi
+                            />
+                        </Col>
+                    </Row>
+                    <Row className="justify-content-center" >
+                        <Col md="4" className={ classes['add-filter-wrapper'] }>
+                            <Button
+                                color="info"
+                            >
+                                +
+                            </Button>
+                        </Col>
+                        <Col md="4" className={ classes['add-filter-wrapper'] }>
+                            <Button
+                                color="info"
+                            >
+                                +
+                            </Button>
+                        </Col>
+                        <Col md="4" className={ classes['add-filter-wrapper'] }>
+                            <Button
+                                color="info"
+                            >
+                                +
+                            </Button>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col md={ { size: 6, offset: 3 } }>
+                            <Input
+                                type="textarea"
+                                name="description"
+                                label="Description"
+                                textarea={ {
+                                    rows: 5
+                                } }
+                            />
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col className="text-center">
+                            <Button
+                                color="success"
+                                type="submit"
+                            >
+                                Save
+                            </Button>
+                        </Col>
+                    </Row>
+                </>
             </Form>
         </>
     );
