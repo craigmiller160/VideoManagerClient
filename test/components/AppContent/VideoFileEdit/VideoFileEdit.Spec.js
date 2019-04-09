9909ce0e00185c9fc28d089cb4b51b2504b8de8e
Craig Miller <craigmiller160@gmail.com>
2019-04-09 17:05:10 -0400

    Stubbed out the tests for VideoFileEdit.Spec

--- test/components/AppContent/VideoFileEdit/VideoFileEdit.Spec.js
+++ test/components/AppContent/VideoFileEdit/VideoFileEdit.Spec.js
@@ -1,6 +1,14 @@
 
 describe('VideoFileEdit', () => {
-    it('finish this', () => {
+    it('renders correctly', () => {
+        throw new Error('Finish this');
+    });
+
+    it('renders an empty div without selected video', () => {
+        throw new Error('Finish this');
+    });
+
+    it('renders prompt when leaving without submitting', () => {
         throw new Error('Finish this');
     });
 });
