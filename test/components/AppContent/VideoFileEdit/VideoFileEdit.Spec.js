50ac2451b6779a64cbb8a180ca6261ff1c6fa135
Craig Miller <craigmiller160@gmail.com>
2019-04-09 20:43:29 -0400

    Commented out incomplete tests

--- test/components/AppContent/VideoFileEdit/VideoFileEdit.Spec.js
+++ test/components/AppContent/VideoFileEdit/VideoFileEdit.Spec.js
@@ -1,14 +1,14 @@
 
-describe('VideoFileEdit', () => {
-    it('renders correctly', () => {
-        throw new Error('Finish this');
-    });
-
-    it('renders an empty div without selected video', () => {
-        throw new Error('Finish this');
-    });
-
-    it('renders prompt when leaving without submitting', () => {
-        throw new Error('Finish this');
-    });
-});
+// describe('VideoFileEdit', () => {
+//     it('renders correctly', () => {
+//         throw new Error('Finish this');
+//     });
+//
+//     it('renders an empty div without selected video', () => {
+//         throw new Error('Finish this');
+//     });
+//
+//     it('renders prompt when leaving without submitting', () => {
+//         throw new Error('Finish this');
+//     });
+// });
