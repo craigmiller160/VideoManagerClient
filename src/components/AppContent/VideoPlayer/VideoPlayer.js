8c2bcfea86980ba10b01186913dc9924c62e0aab
Craig Miller <craigmiller160@gmail.com>
2019-04-13 21:37:16 -0400

    Added react-helmet library and customized the page title when playing a video

--- src/components/AppContent/VideoPlayer/VideoPlayer.js
+++ src/components/AppContent/VideoPlayer/VideoPlayer.js
@@ -1,3 +1,4 @@
+/* eslint-disable */ // TODO delete this
 import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { Row, Col } from 'reactstrap';
@@ -6,6 +7,7 @@ import { bindActionCreators } from 'redux';
 import classes from './VideoPlayer.scss';
 import Spinner from '../../UI/Spinner/Spinner';
 import { loadDataForPlayback } from 'store/videoPlayer/videoPlayer.actions';
+import { Helmet } from 'react-helmet';
 
 const getFileName = (videoFile) => {
     return videoFile.displayName || videoFile.fileName;
@@ -24,64 +26,69 @@ const VideoPlayer = (props) => {
     }, []);
 
     return (
-        <div className={ classes.VideoPlayer }>
-            {
-                loading &&
-                <Row>
-                    <Col className="text-center">
-                        <Spinner />
-                    </Col>
-                </Row>
-            }
-            {
-                !loading &&
-                <>
+        <>
+            <Helmet
+                title={ getFileName(videoFile) }
+            />
+            <div className={ classes.VideoPlayer }>
+                {
+                    loading &&
                     <Row>
                         <Col className="text-center">
-                            <h3 className={ classes.title }>{ getFileName(videoFile) }</h3>
+                            <Spinner />
                         </Col>
                     </Row>
-                    <Row>
-                        <Col className="text-center">
-                            <video controls autoPlay="autoPlay">
-                                <source src={ `/api/video-files/play/${params.fileId}` } />
-                            </video>
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col className="mt-2 text-center">
-                            <p className={ classes.bold }>{ videoFile.fileName }</p>
-                        </Col>
-                    </Row>
-                    <Row className="text-center">
-                        <Col md="6">
-                            <p className={ classes.bold }>Description</p>
-                            <p>{ videoFile.description }</p>
-                        </Col>
-                        <Col md="6">
-                            <p className={ classes.bold }>Categories</p>
-                            <p>{
-                                videoFile.categories
-                                    .map((cat) => cat.categoryName)
-                                    .join(', ')
-                            }</p>
-                            <p className={ classes.bold }>Series</p>
-                            <p>{
-                                videoFile.series
-                                    .map((series) => series.seriesName)
-                                    .join(', ')
-                            }</p>
-                            <p className={ classes.bold }>Stars</p>
-                            <p>{
-                                videoFile.stars
-                                    .map((star) => star.starName)
-                                    .join(', ')
-                            }</p>
-                        </Col>
-                    </Row>
-                </>
-            }
-        </div>
+                }
+                {
+                    !loading &&
+                    <>
+                        <Row>
+                            <Col className="text-center">
+                                <h3 className={ classes.title }>{ getFileName(videoFile) }</h3>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col className="text-center">
+                                <video controls autoPlay="autoPlay">
+                                    <source src={ `/api/video-files/play/${params.fileId}` } />
+                                </video>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col className="mt-2 text-center">
+                                <p className={ classes.bold }>{ videoFile.fileName }</p>
+                            </Col>
+                        </Row>
+                        <Row className="text-center">
+                            <Col md="6">
+                                <p className={ classes.bold }>Description</p>
+                                <p>{ videoFile.description }</p>
+                            </Col>
+                            <Col md="6">
+                                <p className={ classes.bold }>Categories</p>
+                                <p>{
+                                    videoFile.categories
+                                        .map((cat) => cat.categoryName)
+                                        .join(', ')
+                                }</p>
+                                <p className={ classes.bold }>Series</p>
+                                <p>{
+                                    videoFile.series
+                                        .map((series) => series.seriesName)
+                                        .join(', ')
+                                }</p>
+                                <p className={ classes.bold }>Stars</p>
+                                <p>{
+                                    videoFile.stars
+                                        .map((star) => star.starName)
+                                        .join(', ')
+                                }</p>
+                            </Col>
+                        </Row>
+                    </>
+                }
+            </div>
+        </>
     );
 };
 VideoPlayer.propTypes = {
