b7f51478951ddb6f3ab000b20cd80697ad105880
Craig Miller <craigmiller160@gmail.com>
2019-07-14 18:48:23 -0400

    Fixed mobile selection of combo boxes

--- src/components/UI/form/Select/Select.js
+++ src/components/UI/form/Select/Select.js
@@ -28,7 +28,8 @@ export const SelectComponent = (props) => {
                 { ...input }
                 { ...inputProps }
                 name={ input && input.name }
-                onBlur={ () => input.onBlur(input.value) }
+                onBlur={ (event) => event.preventDefault() }
+                onChange={ input.onChange }
                 options={ options }
                 isSearchable
                 isClearable
@@ -45,7 +46,8 @@ SelectComponent.propTypes = {
         value: PropTypes.any
     })),
     input: PropTypes.shape({
-        name: PropTypes.string
+        name: PropTypes.string,
+        onChange: PropTypes.func
     }),
     multi: PropTypes.bool,
     inputProps: PropTypes.object
