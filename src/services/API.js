a055d0e9d6d265611896870d6f4f4815cad552af
Craig Miller <craigmiller160@gmail.com>
2019-08-09 21:21:46 -0400

    Added automatic adding of security token to requests in Auth header

--- src/services/API.js
+++ src/services/API.js
@@ -1,8 +1,22 @@
 import axios from 'axios';
+import { TOKEN_KEY } from '../utils/securityConstants';
 
-export default axios.create({
+const instance = axios.create({
     baseURL: '/api',
     headers: {
         'Content-Type': 'application/json'
     }
 });
+
+instance.interceptors.request.use((config) => {
+    const token = localStorage.getItem(TOKEN_KEY);
+    if (token) {
+        config.headers = {
+            ...config.headers,
+            Authorization: `Bearer ${token}`
+        };
+    }
+    return config;
+});
+
+export default instance;
