c90235d2c5dbbc5f3f0c6686709e38cec798781f
Craig Miller <craigmiller160@gmail.com>
2019-08-31 16:18:10 -0400

    Built some of the login logic

--- src/store/auth/auth.actions.js
+++ src/store/auth/auth.actions.js
@@ -1,5 +1,6 @@
 import { createAction } from 'redux-starter-kit';
 import * as AuthService from 'services/AuthApiService';
+import { TOKEN_KEY } from '../../utils/securityConstants';
 
 export const setIsAuth = createAction('auth/setIsAuth');
 
@@ -12,5 +13,23 @@ export const checkAuth = () => async (dispatch) => {
     catch (ex) {
         dispatch(setIsAuth(false));
         return false;
+    }
+};
+
+/* eslint-disable */
+export const login = () => async (dispatch, getState) => {
+    try {
+        const state = getState();
+        const { username, password } = state?.form?.LoginForm?.values ?? {};
+        if (!username || !password) {
+            return;
+        }
+
+        const result = await AuthService.login();
+        localStorage.setItem(TOKEN_KEY, result.data.token);
+        dispatch(setIsAuth(true));
+    }
+    catch (ex) {
+        dispatch(setIsAuth(false));
     }
 };
