e19289e91f051689a4c56a89325085cca8dd5e88
Craig Miller <craigmiller160@gmail.com>
2019-08-31 19:08:41 -0400

    Integrated loading into the Login page

--- src/components/AppContent/Login/Login.js
+++ src/components/AppContent/Login/Login.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector, useDispatch, shallowEqual } from 'react-redux';
 import { Button, Col, Row } from 'reactstrap';
 import Form from 'components/UI/form/Form/Form';
 import Input from 'components/UI/form/Input/Input';
@@ -7,12 +7,14 @@ import classes from './Login.scss';
 import { isRequired, isValidEmail } from '../../../utils/validations';
 import { login } from '../../../store/auth/auth.actions';
 import { loginFormHasErrors } from '../../../store/auth/auth.selectors';
+import Spinner from '../../UI/Spinner/Spinner';
 
 export const LOGIN_FORM_NAME = 'LoginForm';
 
 const Login = () => {
     const dispatch = useDispatch();
     const loginFormErrors = useSelector(loginFormHasErrors);
+    const loginLoading = useSelector((state) => state.auth.loginLoading, shallowEqual);
 
     return (
         <Form
@@ -20,45 +22,52 @@ const Login = () => {
             onSubmit={ (values) => dispatch(login(values)) }
             className={ classes.Login }
         >
-            <>
-                <Row>
-                    <Col className="text-center" md={ { size: 4, offset: 4 } }>
-                        <h3>Login</h3>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col md={ { size: 4, offset: 4 } }>
-                        <Input
-                            label="Username"
-                            name="userName"
-                            type="email"
-                            validate={ [
-                                isRequired,
-                                isValidEmail
-                            ] }
-                        />
-                        <Input
-                            label="Password"
-                            name="password"
-                            type="password"
-                            validate={ [
-                                isRequired
-                            ] }
-                        />
-                    </Col>
-                </Row>
-                <Row>
-                    <Col className="text-center" md={ { size: 4, offset: 4 } }>
-                        <Button
-                            type="submit"
-                            color="primary"
-                            disabled={ loginFormErrors }
-                        >
-                            Login
-                        </Button>
-                    </Col>
-                </Row>
-            </>
+            {
+                loginLoading &&
+                    <Spinner />
+            }
+            {
+                !loginLoading &&
+                <>
+                    <Row>
+                        <Col className="text-center" md={ { size: 4, offset: 4 } }>
+                            <h3>Login</h3>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col md={ { size: 4, offset: 4 } }>
+                            <Input
+                                label="Username"
+                                name="userName"
+                                type="email"
+                                validate={ [
+                                    isRequired,
+                                    isValidEmail
+                                ] }
+                            />
+                            <Input
+                                label="Password"
+                                name="password"
+                                type="password"
+                                validate={ [
+                                    isRequired
+                                ] }
+                            />
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col className="text-center" md={ { size: 4, offset: 4 } }>
+                            <Button
+                                type="submit"
+                                color="primary"
+                                disabled={ loginFormErrors }
+                            >
+                                Login
+                            </Button>
+                        </Col>
+                    </Row>
+                </>
+            }
         </Form>
     );
 };
