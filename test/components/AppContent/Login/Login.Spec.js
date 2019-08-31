2e63f733e787eec741c6a946abf2a8d11e4cb1e8
Craig Miller <craigmiller160@gmail.com>
2019-08-31 16:51:12 -0400

    Stubbed out Login component unit tests

---
+++ test/components/AppContent/Login/Login.Spec.js
@@ -0,0 +1,14 @@
+
+describe('Login', () => {
+    it('renders correctly', () => {
+        throw new Error('Finish this');
+    });
+
+    it('disables login button on errors', () => {
+        throw new Error('Finish this');
+    });
+
+    it('submits form', () => {
+        throw new Error('Finish this');
+    });
+});
