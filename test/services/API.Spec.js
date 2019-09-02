9f0bb409eb268a5438ff744ff4f29245595d062f
Craig Miller <craigmiller160@gmail.com>
2019-09-01 21:41:33 -0400

    Updated API unit tests

--- test/services/API.Spec.js
+++ test/services/API.Spec.js
@@ -1,42 +1,70 @@
-import { TOKEN_KEY } from '../../src/utils/securityConstants';
 import { addTokenInterceptor } from '../../src/services/API';
+import { CSRF_TOKEN_KEY } from 'utils/securityConstants';
+import { mockCsrfToken } from '../exclude/mock/mockApiConfig/authApi';
+
+jest.mock('store/store', () => {
+    let csrfToken = '';
+
+    return {
+        getState() {
+            return {
+                auth: {
+                    csrfToken
+                }
+            }
+        },
+        setCsrfToken(token) {
+            csrfToken = token
+        },
+        clearCsrfToken() {
+            csrfToken = '';
+        }
+    }
+});
+
+import store from 'store/store'; // eslint-disable-line import/first
 
 const contentType = 'Content-Type';
 const applicationJson = 'application/json';
 
 describe('API', () => {
     describe('addTokenInterceptor', () => {
+        const config = {
+            method: 'post',
+            headers: {
+                [contentType]: applicationJson
+            }
+        };
+
         beforeEach(() => {
-            localStorage.clear();
+            store.clearCsrfToken();
         });
 
         it('adds token if exists', () => {
-            const token = 'MyToken';
-            localStorage.setItem(TOKEN_KEY, token);
-            const config = {
-                headers: {
-                    [contentType]: applicationJson
-                }
-            };
+            store.setCsrfToken(mockCsrfToken);
             const result = addTokenInterceptor(config);
             expect(result).toEqual({
+                ...config,
                 headers: {
-                    [contentType]: applicationJson,
-                    Authorization: `Bearer ${token}`
+                    ...config.headers,
+                    [CSRF_TOKEN_KEY]: mockCsrfToken
                 }
             });
         });
 
         it('does not add token if not exists', () => {
-            const config = {
-                headers: {
-                    [contentType]: applicationJson
-                }
-            };
             const result = addTokenInterceptor(config);
+            expect(result).toEqual(config);
+        });
+
+        it('does not add token if method is get', () => {
+            store.setCsrfToken(mockCsrfToken);
+            const result = addTokenInterceptor({ ...config, method: 'get' });
             expect(result).toEqual({
+                ...config,
+                method: 'get',
                 headers: {
-                    [contentType]: applicationJson
+                    ...config.headers
                 }
             });
         });
