078e3066a51fa11d3c8c4e037f9d8554f57f3ad4
Craig Miller <craigmiller160@gmail.com>
2019-09-15 16:36:52 -0400

    Fixed VideoSearch.Spec

--- test/components/AppContent/VideoListLayout/VideoSearch/VideoSearch.Spec.js
+++ test/components/AppContent/VideoListLayout/VideoSearch/VideoSearch.Spec.js
@@ -1,67 +1,88 @@
 import React from 'react';
 import { mount } from 'enzyme';
-import { FORM_NAME, VideoSearch } from 'components/AppContent/VideoListLayout/VideoSearch/VideoSearch';
-import mockComponent from '../../../../exclude/testUtil/mockComponent';
+import VideoSearch, { FORM_NAME } from 'components/AppContent/VideoListLayout/VideoSearch/VideoSearch';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
 
-jest.mock('redux-form', () => ({
-    reduxForm: () => (Comp) => Comp
+jest.mock('components/UI/form/Input/Input', () => {
+    const Input = (props) => <div>{ props.children }</div>;
+    return Input;
+});
+jest.mock('components/UI/form/Select/Select', () => {
+    const Select = (props) => <div>{ props.children }</div>;
+    return Select;
+});
+jest.mock('components/UI/form/Form/Form', () => {
+    const Form = (props) => <div>{ props.children }</div>;
+    return Form;
+});
+
+jest.mock('store/videoList/videoList.actions', () => ({
+    searchForVideos: () => ({ type: 'SearchForVideos' }),
+    setCurrentPage: (page) => ({ type: 'SetCurrentPage', payload: page })
 }));
-jest.mock('components/UI/form/Input/Input', () => (props) => mockComponent('Input', props));
-jest.mock('components/UI/form/Select/Select', () => (props) => mockComponent('Select', props));
 
-const loadFilterOptions = jest.fn();
-const searchForVideos = jest.fn();
-const resetForm = jest.fn();
-const setCurrentPage = jest.fn();
-const props = {
-    loadFilterOptions,
-    searchForVideos,
-    resetForm,
-    setCurrentPage,
-    filters: {
-        categories: [
-            { value: 1, label: 'Cat 1' }
-        ],
-        series: [
-            { value: 1, label: 'Series 1' }
-        ],
-        stars: [
-            { value: 1, label: 'Star 1' }
-        ]
+const mockStore = configureMockStore([thunk]);
+
+const defaultState = {
+    videoSearch: {
+        filters: {
+            categories: [
+                { value: 1, label: 'Cat 1' }
+            ],
+            series: [
+                { value: 1, label: 'Series 1' }
+            ],
+            stars: [
+                { value: 1, label: 'Star 1' }
+            ]
+        }
     }
 };
 
-const createComponent = (props = {}) => mount(
-    <VideoSearch { ...props } />
-);
+const doMount = (state = defaultState) => {
+    const store = mockStore(state);
+    const component = mount(
+        <Provider store={ store }>
+            <VideoSearch />
+        </Provider>
+    );
+    return [component, store];
+};
 
 describe('VideoSearch', () => {
-    beforeEach(() => {
-        loadFilterOptions.mockReset();
-        searchForVideos.mockReset();
-        resetForm.mockReset();
-        setCurrentPage.mockReset();
-    });
-
     it('renders properly', () => {
-        const component = createComponent(props);
-        expect(component.find('mock-comp[name="category"]').props()).toEqual(expect.objectContaining({
-            options: props.filters.categories
+        const [component] = doMount();
+        expect(component.find('Select[name="category"]').props()).toEqual(expect.objectContaining({
+            options: defaultState.videoSearch.filters.categories
         }));
-        expect(component.find('mock-comp[name="series"]').props()).toEqual(expect.objectContaining({
-            options: props.filters.series
+        expect(component.find('Select[name="series"]').props()).toEqual(expect.objectContaining({
+            options: defaultState.videoSearch.filters.series
         }));
-        expect(component.find('mock-comp[name="star"]').props()).toEqual(expect.objectContaining({
-            options: props.filters.stars
+        expect(component.find('Select[name="star"]').props()).toEqual(expect.objectContaining({
+            options: defaultState.videoSearch.filters.stars
         }));
     });
 
     it('calls actions on click', () => {
-        const component = createComponent(props);
-        component.find(`Form[form="${FORM_NAME}"]`).simulate('submit');
-        expect(setCurrentPage).toHaveBeenCalledWith(0);
-        expect(searchForVideos).toHaveBeenCalled();
+        const [component, store] = doMount();
+
+        const actions1 = [
+            { type: 'SetCurrentPage', payload: 0 },
+            { type: 'SearchForVideos' }
+        ];
+
+        component.find(`Form[form="${FORM_NAME}"]`).props().onSubmit();
+        expect(store.getActions()).toEqual(actions1);
+
+        const actions2 = [
+            ...actions1,
+            { type: '@@redux-form/RESET', meta: { form: 'video-search' } },
+            { type: 'SearchForVideos' }
+        ];
+
         component.find('button#reset-video-search-btn').simulate('click');
-        expect(resetForm).toHaveBeenLastCalledWith(FORM_NAME);
+        expect(store.getActions()).toEqual(actions2);
     });
 });
