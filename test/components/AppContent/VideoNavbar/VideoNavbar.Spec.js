489b97eecf61680ce0336d1b75a3a20d2a6f55b1
Craig Miller <craigmiller160@gmail.com>
2019-09-15 16:46:44 -0400

    Began fixing VideoNavbar.Spec

--- test/components/AppContent/VideoNavbar/VideoNavbar.Spec.js
+++ test/components/AppContent/VideoNavbar/VideoNavbar.Spec.js
@@ -2,63 +2,103 @@ import React from 'react';
 import { mount } from 'enzyme';
 import VideoNavbar from 'components/AppContent/VideoNavbar/VideoNavbar';
 import { MemoryRouter } from 'react-router';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
 
-jest.mock('react-router-dom', () => ({ Link: () => 'Link' }));
+// jest.mock('react-router-dom', () => ({ Link: () => 'Link' }));
 
-const mountComponent = (props = {}) => {
-    return mount(
-        <MemoryRouter initialEntries={ ['/'] }>
-            <VideoNavbar { ...props } />
-        </MemoryRouter>
-    );
+const mockStore = configureMockStore([thunk]);
+
+const defaultProps = {
+    disabled: false
 };
 
-const startFileScan = jest.fn();
-const props = {
-    startFileScan,
-    disabled: false
+const doMount = (props = defaultProps) => {
+    const store = mockStore({});
+    const component = mount(
+        <Provider store={ store }>
+            <MemoryRouter initialEntries={ ['/'] }>
+                <VideoNavbar { ...props } />
+            </MemoryRouter>
+        </Provider>
+    );
+    return [component, store];
 };
 
 describe('VideoNavbar', () => {
     it('renders successfully', () => {
-        const component = mountComponent(props);
+        const [component] = doMount();
         expect(component.find('VideoNavbar')).toHaveLength(1);
         expect(component.find('NavbarBrand')).toHaveLength(1);
-        expect(component.find('NavItem')).toHaveLength(3);
+        expect(component.find('NavItem')).toHaveLength(4);
         expect(component.find('NavbarToggler')).toHaveLength(1);
     });
 
-    it('hides/disables items if scanning', () => {
-        const component = mountComponent({
-            ...props,
-            disabled: true
-        });
-        expect(component.find('VideoNavbar')).toHaveLength(1);
-        expect(component.find('NavItem')).toHaveLength(0);
-        expect(component.find('NavbarToggler')).toHaveLength(0);
-        expect(component.find('NavbarBrand')).toHaveLength(1);
-        expect(component.find('NavbarBrand').props()).toEqual(expect.objectContaining({
-            disabled: true
-        }));
+    it('hides/disables items', () => {
+        throw new Error('Finish this');
     });
 
     it('toggles the collapse open and closed', () => {
-        const component = mountComponent(props);
-        const toggle = component.find('VideoNavbar NavbarToggler');
-
-        expect(component.find('Collapse').props()).toEqual(expect.objectContaining({
-            isOpen: false
-        }));
-        toggle.simulate('click');
-        expect(component.find('Collapse').props()).toEqual(expect.objectContaining({
-            isOpen: true
-        }));
+        throw new Error('Finish this');
     });
 
-    it('Starts file scan', () => {
-        const component = mountComponent(props);
-        const scanDirectoryBtn = component.find('NavLink#scanDirectory');
-        scanDirectoryBtn.simulate('click');
-        expect(startFileScan).toHaveBeenCalled();
+    describe('click actions', () => {
+        it('clicks on navbar brand', () => {
+            throw new Error('Finish this');
+        });
+
+        it('clicks on video list link', () => {
+            throw new Error('Finish this');
+        });
+
+        it('clicks on filters link', () => {
+            throw new Error('Finish this');
+        });
+
+        it('clicks on scan directory link', () => {
+            throw new Error('Finish this');
+        });
+
+        it('clicks on logout link', () => {
+            throw new Error('Finish this');
+        });
     });
+
+
+
+
+    // it('hides/disables items if scanning', () => {
+    //     const component = mountComponent({
+    //         ...props,
+    //         disabled: true
+    //     });
+    //     expect(component.find('VideoNavbar')).toHaveLength(1);
+    //     expect(component.find('NavItem')).toHaveLength(0);
+    //     expect(component.find('NavbarToggler')).toHaveLength(0);
+    //     expect(component.find('NavbarBrand')).toHaveLength(1);
+    //     expect(component.find('NavbarBrand').props()).toEqual(expect.objectContaining({
+    //         disabled: true
+    //     }));
+    // });
+    //
+    // it('toggles the collapse open and closed', () => {
+    //     const component = mountComponent(props);
+    //     const toggle = component.find('VideoNavbar NavbarToggler');
+    //
+    //     expect(component.find('Collapse').props()).toEqual(expect.objectContaining({
+    //         isOpen: false
+    //     }));
+    //     toggle.simulate('click');
+    //     expect(component.find('Collapse').props()).toEqual(expect.objectContaining({
+    //         isOpen: true
+    //     }));
+    // });
+    //
+    // it('Starts file scan', () => {
+    //     const component = mountComponent(props);
+    //     const scanDirectoryBtn = component.find('NavLink#scanDirectory');
+    //     scanDirectoryBtn.simulate('click');
+    //     expect(startFileScan).toHaveBeenCalled();
+    // });
 });
