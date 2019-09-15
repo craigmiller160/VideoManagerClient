58438d6200eddf162b9843f9668ffd202958fe54
Craig Miller <craigmiller160@gmail.com>
2019-09-15 07:55:34 -0400

    More tests

--- test/services/API.Spec.js
+++ test/services/API.Spec.js
@@ -1,10 +1,10 @@
-import { addCsrfTokenInterceptor } from '../../src/services/API';
+import API, { addCsrfTokenInterceptor, handle401Interceptor } from '../../src/services/API';
 import { CSRF_TOKEN_KEY } from 'utils/securityConstants';
 import { mockCsrfToken } from '../exclude/mock/mockApiConfig/authApi';
-import store from 'store/store'; // eslint-disable-line import/first
 
 jest.mock('store/store', () => {
     let csrfToken = '';
+    let actions = [];
 
     return {
         getState() {
@@ -19,14 +19,34 @@ jest.mock('store/store', () => {
         },
         clearCsrfToken() {
             csrfToken = '';
+        },
+        dispatch(action) {
+            actions.push(action);
+        },
+        getActions() {
+            return actions;
+        },
+        clearActions() {
+            actions = [];
         }
     }
 });
 
+import store from 'store/store';
+import MockAdapter from 'axios-mock-adapter'; // eslint-disable-line import/first
+
 const contentType = 'Content-Type';
 const applicationJson = 'application/json';
 
+const mockApi = new MockAdapter(API);
+
 describe('API', () => {
+    beforeEach(() => {
+        store.clearCsrfToken();
+        store.clearActions();
+        mockApi.reset();
+    });
+
     describe('addCsrfTokenInterceptor', () => {
         const config = {
             method: 'post',
@@ -34,10 +54,6 @@ describe('API', () => {
                 [contentType]: applicationJson
             }
         };
-
-        beforeEach(() => {
-            store.clearCsrfToken();
-        });
 
         it('adds token if exists', () => {
             store.setCsrfToken(mockCsrfToken);
@@ -70,8 +86,23 @@ describe('API', () => {
     });
 
     describe('handle401Interceptor', () => {
-        it('does refresh for 401', () => {
-            throw new Error('Finish this');
+        it('does refresh for 401', async () => {
+            mockApi.onGet('/auth/refresh')
+                .reply(204);
+            mockApi.onGet('/video-files')
+                .reply(200, 'Success');
+
+            const error = {
+                response: {
+                    status: 401
+                },
+                config: {
+                    url: '/api/video-files'
+                }
+            };
+            const res = await handle401Interceptor(error);
+            expect(res.data).toEqual('Success');
+            // TODO might want to do more tests for what happened along the way
         });
 
         it('skips refresh on 401 from refresh uri', () => {
@@ -79,6 +110,10 @@ describe('API', () => {
         });
 
         it('has error during refresh', () => {
+            throw new Error('Finish this');
+        });
+
+        it('has error that is not noAuth', () => {
             throw new Error('Finish this');
         });
     });
