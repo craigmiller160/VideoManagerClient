68e317ed4f72c5cf8e5a661c818a3f49e970f5d1
Craig Miller <craigmiller160@gmail.com>
2019-10-12 15:47:47 -0400

    Implemented save user profile

--- src/store/auth/auth.actions.js
+++ src/store/auth/auth.actions.js
@@ -1,12 +1,14 @@
 import { createAction } from 'redux-starter-kit';
 import * as AuthService from 'services/AuthApiService';
 import { CSRF_TOKEN_KEY } from '../../utils/securityConstants';
-import { showErrorAlert } from '../alert/alert.actions';
+import { showErrorAlert, showSuccessAlert } from '../alert/alert.actions';
 
 export const setIsAuth = createAction('auth/setIsAuth');
 export const setLoginLoading = createAction('auth/setLoginLoading');
 export const setCsrfToken = createAction('auth/setCsrfToken');
 export const setUserDetails = createAction('auth/setUserDetails');
+
+const formatRoles = (roles) => roles.map((role) => ({ roleId: role.value, name: role.label }));
 
 export const handleCsrfToken = (response) => (dispatch) => {
     const csrfToken = response?.headers?.[CSRF_TOKEN_KEY];
@@ -53,7 +55,18 @@ export const logout = () => async (dispatch) => {
     dispatch(setUserDetails(null));
 };
 
-/* eslint-disable */ // TODO delete this
 export const saveUserProfile = (values) => async (dispatch) => {
-    console.log(values); // TODO delete this
+    const payload = {
+        ...values,
+        roles: formatRoles(values.roles),
+        lastAuthenticated: undefined
+    };
+
+    try {
+        await AuthService.saveUserProfile(payload);
+        await dispatch(checkAuth());
+        dispatch(showSuccessAlert('Successfully saved user profile'));
+    } catch (ex) {
+        dispatch(showErrorAlert(ex.message));
+    }
 };
