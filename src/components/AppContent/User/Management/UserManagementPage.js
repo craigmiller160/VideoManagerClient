b4c013154196d7b2464947cc02f60bbdf952b24e
Craig Miller <craigmiller160@gmail.com>
2019-10-12 16:23:40 -0400

    Integrated GetAllUsers into UserManagementPage

--- src/components/AppContent/User/Management/UserManagementPage.js
+++ src/components/AppContent/User/Management/UserManagementPage.js
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import * as AuthApiService from 'services/AuthApiService';
 import classes from './UserManagementPage.scss';
 import FlexRow from '../../../UI/Grid/FlexRow';
 
 const UserManagementPage = () => {
+    const [allUsers, setAllUsers] = useState([]);
+
+    useEffect(() => {
+        const loadUsers = async () => {
+            const res = await AuthApiService.getAllUsers();
+            setAllUsers(res.data);
+        };
+        loadUsers();
+    }, []);
+
     return (
         <div className={ classes.UserManagementPage }>
             <FlexRow>
                 <div className={ classes.title }>
                     <h3>User Management</h3>
                 </div>
+            </FlexRow>
+            <FlexRow>
+                {
+                    allUsers?.map((user) => user.userName)
+                }
             </FlexRow>
         </div>
     );
