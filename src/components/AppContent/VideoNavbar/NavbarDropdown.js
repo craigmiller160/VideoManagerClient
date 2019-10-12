8b42a1b0c38569eccc454afbb7fd07ed85278061
Craig Miller <craigmiller160@gmail.com>
2019-10-12 15:20:32 -0400

    Added active indicator to dropdown toggle

--- src/components/AppContent/VideoNavbar/NavbarDropdown.js
+++ src/components/AppContent/VideoNavbar/NavbarDropdown.js
@@ -1,19 +1,26 @@
 import React from 'react';
 import { DropdownMenu, DropdownToggle, UncontrolledDropdown } from 'reactstrap';
 import { shallowEqual, useDispatch, useSelector } from 'react-redux';
+import useReactRouter from 'use-react-router';
 import classes from './NavbarDropdown.scss';
 import NavbarItem from './NavbarItem';
 import { logout } from '../../../store/auth/auth.actions';
 
 const NavbarDropdown = () => {
     const dispatch = useDispatch();
+    const { location } = useReactRouter();
     const { firstName, lastName } = useSelector(state => state.auth.userDetails, shallowEqual);
+
+    const toggleClasses = [];
+    if (location.pathname === '/profile') {
+        toggleClasses.push('active');
+    }
 
     const fullName = `${firstName} ${lastName}`;
 
     return (
         <UncontrolledDropdown nav inNavbar className={ classes.NavbarDropdown }>
-            <DropdownToggle nav caret>{ fullName }</DropdownToggle>
+            <DropdownToggle nav caret className={ toggleClasses.join(' ') }>{ fullName }</DropdownToggle>
             <DropdownMenu right className={ classes.dropdown }>
                 <NavbarItem
                     id="userProfileLink"
