0ad07b6a760fcac521371c9252e97b20b3ccf56a
Craig Miller <craigmiller160@gmail.com>
2019-10-13 12:09:11 -0400

    Got a lot of the styles for the user management cards working

--- src/components/AppContent/User/Management/UserListItem.js
+++ src/components/AppContent/User/Management/UserListItem.js
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import classes from './UserListItem.scss';
+import FlexRow from '../../../UI/Grid/FlexRow';
 
 const UserListItem = (props) => {
     const {
         user
     } = props;
 
+    const fullName = `${user.firstName} ${user.lastName}`;
+
     return (
-        <div className={ classes.UserListItem }>
+        <FlexRow
+            justifyContent="space-around"
+            className={ classes.UserListItem }
+        >
             <p>{ user.userName }</p>
-        </div>
+            <p>{ fullName }</p>
+            <p>
+                {
+                    user.roles.map((role) => (
+                        <Fragment key={ role.roleId }>
+                            <span key={ role.roleId }>{ role.name }</span>
+                            <br />
+                        </Fragment>
+                    ))
+                }
+            </p>
+        </FlexRow>
     );
 };
 UserListItem.propTypes = {
