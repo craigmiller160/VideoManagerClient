eb81e40ab4ca337e002262e7f9b3acfb490e5049
Craig Miller <craigmiller160@gmail.com>
2019-10-19 18:13:52 -0400

    Refactored away the need for UserDetailsForm

--- src/components/AppContent/User/UserDetails/UserDetailsForm.js
+++ src/components/AppContent/User/UserDetails/UserDetailsForm.js
@@ -8,7 +8,7 @@ import Select from '../../../UI/form/Select/Select';
 import { Button } from 'reactstrap';
 import Form from '../../../UI/form/Form/Form';
 
-const USER_DETAILS_FORM_NAME = 'UserDetailsForm';
+
 
 const UserDetailsForm = (props) => {
     const {
@@ -19,73 +19,10 @@ const UserDetailsForm = (props) => {
         revokeUser
     } = props;
 
-    const disableRoles = !allRoles || allRoles.length === 0;
+
 
     return (
-        <Form
-            form={ USER_DETAILS_FORM_NAME }
-            onSubmit={ saveUser }
-            className={ classes.UserDetailsForm }
-            initialValues={ initValues }
-        >
-            <FlexRow className="mt-3" justifyContent="space-around">
-                <Input
-                    label="Username"
-                    name="userName"
-                    type="email"
-                    disabled
-                    divClassName={ classes.Input }
-                />
-                <Input
-                    label="Password"
-                    name="password"
-                    type="password"
-                    divClassName={ classes.Input }
-                />
-            </FlexRow>
-            <FlexRow justifyContent="space-around">
-                <Input
-                    label="First Name"
-                    name="firstName"
-                    type="text"
-                    divClassName={ classes.Input }
-                />
-                <Input
-                    label="Last Name"
-                    name="lastName"
-                    type="text"
-                    divClassName={ classes.Input }
-                />
-            </FlexRow>
-            <FlexRow justifyContent="space-around">
-                <Select
-                    label="Roles"
-                    name="roles"
-                    divClassName={ classes.Input }
-                    multi
-                    disabled={ disableRoles }
-                    options={ allRoles }
-                />
-                <Input
-                    label="Last Authenticated"
-                    name="lastAuthenticated"
-                    type="text"
-                    divClassName={ classes.Input }
-                    disabled
-                />
-            </FlexRow>
-            <FlexRow className="mt-5" justifyContent="space-around">
-                {
-                    revokeUser &&
-                    <Button color="info">Revoke Login</Button>
-                }
-                <Button color="primary">Save Changes</Button>
-                {
-                    deleteUser &&
-                    <Button color="danger">Delete User</Button>
-                }
-            </FlexRow>
-        </Form>
+        <div />
     )
 };
 UserDetailsForm.propTypes = {
--- src/components/AppContent/User/UserDetails/UserDetailsPage.js
+++ src/components/AppContent/User/UserDetails/UserDetailsPage.js
@@ -9,11 +9,17 @@ import UserDetailsForm from './UserDetailsForm';
 import Spinner from '../../../UI/Spinner/Spinner';
 import classes from './UserDetailsPage.scss';
 import FlexRow from '../../../UI/Grid/FlexRow';
+import Input from '../../../UI/form/Input/Input';
+import Select from '../../../UI/form/Select/Select';
+import { Button } from 'reactstrap';
+import Form from '../../../UI/form/Form/Form';
 
 // TODO for the roles dropdown, admin users need to load all the roles from the server, then select only the ones from the user
 // TODO revoke login needs to be made to work
 // TODO need a saveUserDetails alternative to saveUserProfile
 // TODO delete user needs to be made to work
+
+const USER_DETAILS_FORM_NAME = 'UserDetailsForm';
 
 const UserDetailsPage = (props) => {
     const {
@@ -25,6 +31,8 @@ const UserDetailsPage = (props) => {
         deleteUser,
         revokeUser
     } = props;
+
+    const disableRoles = !roles || roles.length === 0;
 
     return (
         <div className={ classes.UserDetailsPage }>
@@ -39,13 +47,70 @@ const UserDetailsPage = (props) => {
             }
             {
                 !loading &&
-                <UserDetailsForm
-                    deleteUser={ deleteUser }
-                    revokeUser={ revokeUser }
-                    allRoles={ roles }
-                    initValues={ userDetails }
-                    saveUser={ saveUser }
-                />
+                <Form
+                    form={ USER_DETAILS_FORM_NAME }
+                    onSubmit={ saveUser }
+                    className={ classes.UserDetailsForm }
+                    initialValues={ userDetails }
+                >
+                    <FlexRow className="mt-3" justifyContent="space-around">
+                        <Input
+                            label="Username"
+                            name="userName"
+                            type="email"
+                            disabled
+                            divClassName={ classes.Input }
+                        />
+                        <Input
+                            label="Password"
+                            name="password"
+                            type="password"
+                            divClassName={ classes.Input }
+                        />
+                    </FlexRow>
+                    <FlexRow justifyContent="space-around">
+                        <Input
+                            label="First Name"
+                            name="firstName"
+                            type="text"
+                            divClassName={ classes.Input }
+                        />
+                        <Input
+                            label="Last Name"
+                            name="lastName"
+                            type="text"
+                            divClassName={ classes.Input }
+                        />
+                    </FlexRow>
+                    <FlexRow justifyContent="space-around">
+                        <Select
+                            label="Roles"
+                            name="roles"
+                            divClassName={ classes.Input }
+                            multi
+                            disabled={ disableRoles }
+                            options={ roles }
+                        />
+                        <Input
+                            label="Last Authenticated"
+                            name="lastAuthenticated"
+                            type="text"
+                            divClassName={ classes.Input }
+                            disabled
+                        />
+                    </FlexRow>
+                    <FlexRow className="mt-5" justifyContent="space-around">
+                        {
+                            revokeUser &&
+                            <Button color="info">Revoke Login</Button>
+                        }
+                        <Button color="primary">Save Changes</Button>
+                        {
+                            deleteUser &&
+                            <Button color="danger">Delete User</Button>
+                        }
+                    </FlexRow>
+                </Form>
             }
         </div>
     );
