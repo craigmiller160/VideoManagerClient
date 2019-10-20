598ad0b2328b08129d723e8a0aa715915fa57fb7
Craig Miller <craigmiller160@gmail.com>
2019-10-20 11:44:10 -0400

    Wrote tests for userUtils

--- test/components/AppContent/User/UserDetails/userUtils.Spec.js
+++ test/components/AppContent/User/UserDetails/userUtils.Spec.js
@@ -1,5 +1,59 @@
+import { formatRoles, formatUser, unFormatRoles } from 'components/AppContent/User/UserDetails/userUtils';
+
+const standardRoles = [
+    { roleId: 1, name: 'one' },
+    { roleId: 2, name: 'two' }
+];
+
+const formattedRoles = [
+    { value: 1, label: 'one' },
+    { value: 2, label: 'two' }
+];
+
+const standardUser = {
+    userId: 1,
+    userName: 'userName',
+    firstName: 'firstName',
+    lastName: 'lastName',
+    roles: standardRoles
+};
+
+const standardTimestamp = '2019-01-01T01:01:01.111';
+const formattedTimestamp = '2019-01-01 01:01:01.111';
+
 describe('userUtils', () => {
-    it('create tests', () => {
-        throw new Error('Finish this');
+    it('formatRoles', () => {
+        const result = formatRoles(standardRoles);
+        expect(result).toEqual(formattedRoles);
+    });
+
+    describe('formatUser', () => {
+        it('with lastAuthenticated', () => {
+            const user = {
+                ...standardUser,
+                lastAuthenticated: standardTimestamp
+            };
+
+            const result = formatUser(user);
+            expect(result).toEqual({
+                ...standardUser,
+                roles: formattedRoles,
+                lastAuthenticated: formattedTimestamp
+            });
+        });
+
+        it('without lastAuthenticated', () => {
+            const result = formatUser(standardUser);
+            expect(result).toEqual({
+                ...standardUser,
+                roles: formattedRoles,
+                lastAuthenticated: ''
+            });
+        });
+    });
+
+    it('unFormatRoles', () => {
+        const result = unFormatRoles(formattedRoles);
+        expect(result).toEqual(standardRoles);
     });
 });
