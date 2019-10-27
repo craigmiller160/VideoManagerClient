7328435a6b24aa07517b16524a900605c815104d
Craig Miller <craigmiller160@gmail.com>
2019-10-27 18:16:26 -0400

    Stubbed out tests for UserManagementPage

--- test/components/AppContent/User/UserManagement/UserManagementPage.Spec.js
+++ test/components/AppContent/User/UserManagement/UserManagementPage.Spec.js
@@ -1,5 +1,36 @@
+import UserManagementPage from 'components/AppContent/User/Management/UserManagementPage';
+import mountTestComponent from '../../../../exclude/testUtil/mountTestComponent';
+
+const defaultProps = {
+    history: {
+        push: jest.fn()
+    }
+};
+
+const doMount = mountTestComponent(UserManagementPage, {
+    defaultProps
+});
+
 describe('UserManagementPage', () => {
-    it('write unit tests', () => {
-        throw new Error();
+    beforeEach(() => {
+        jest.clearAllMocks();
+    });
+
+    describe('rendering', () => {
+        it('renders all users', () => {
+            // TODO don't forget about loading users from mock server
+            // TODO don't forget about user sorting
+            throw new Error('Finish this');
+        });
+    });
+
+    describe('callbacks and actions', () => {
+        it('calls history.push when clicking add button', () => {
+            throw new Error('Finish this');
+        });
+
+        it('updates state in changeExpanded', () => {
+            throw new Error('Finish this');
+        });
     });
 });
