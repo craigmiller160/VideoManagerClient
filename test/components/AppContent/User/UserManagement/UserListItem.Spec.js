72286be375a32d3986476f8a74138a1f786575ab
Craig Miller <craigmiller160@gmail.com>
2019-10-27 17:56:30 -0400

    Stubbed out UserListItem tests

--- test/components/AppContent/User/UserManagement/UserListItem.Spec.js
+++ test/components/AppContent/User/UserManagement/UserListItem.Spec.js
@@ -1,5 +1,41 @@
+import UserListItem from 'components/AppContent/User/Management/UserListItem';
+import mountTestComponent from '../../../../exclude/testUtil/mountTestComponent';
+import { ROLE_ADMIN, ROLE_EDIT } from '../../../../../src/utils/securityConstants';
+
+const changeExpanded = jest.fn();
+const defaultProps = {
+    user: {
+        userId: 1
+        firstName: 'firstName',
+        lastName: 'lastName',
+        userName: 'userName',
+        expanded: false,
+        roles: [
+            { roleId: 1, name: ROLE_EDIT },
+            { roleId: 2, name: ROLE_ADMIN }
+        ]
+    },
+    changeExpanded
+};
+
+const doMount = mountTestComponent(UserListItem, {
+    defaultProps
+});
+
 describe('UserListItem', () => {
-    it('write unit tests', () => {
-        throw new Error();
+    describe('rendering', () => {
+        it('renders without expanded', () => {
+            throw new Error('Finish this');
+        });
+
+        it('renders with expanded', () => {
+            throw new Error('Finish this');
+        });
+    });
+
+    describe('callbacks', () => {
+        it('calls changeExpanded', () => {
+            throw new Error('Finish this');
+        });
     });
 });
