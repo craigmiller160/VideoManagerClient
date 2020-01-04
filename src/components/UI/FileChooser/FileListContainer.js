c905460c833ff682f83166612a5ef1c058102a02
Craig Miller <craigmiller160@gmail.com>
2020-01-04 11:11:22 -0500

    Stubbed out file chooser logic

---
+++ src/components/UI/FileChooser/FileListContainer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const FileListContainer = (props) => {
+    return (
+        <div />
+    );
+};
+FileListContainer.propTypes = {
+
+};
+
+export default FileListContainer;
---
+++ src/components/UI/FileChooser/FileListItem.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const FileListItem = (props) => {
+    return (
+        <div />
+    );
+};
+FileListItem.propTypes = {
+
+};
+
+export default FileListItem;
