b7f4d370a9b48d832234dbfe0dcac5917356ba29
Craig Miller <craigmiller160@gmail.com>
2020-01-04 12:19:02 -0500

    Began laying out more logic for the file list

--- src/components/UI/FileChooser/FileListContainer.js
+++ src/components/UI/FileChooser/FileListContainer.js
@@ -1,6 +1,7 @@
 /* eslint-disable */ // TODO delete this
 import React from 'react';
 import PropTypes from 'prop-types';
+import classes from './FileListContainer.scss';
 
 const FileListContainer = (props) => {
     const {
@@ -8,13 +9,13 @@ const FileListContainer = (props) => {
     } = props;
 
     return (
-        <div>
+        <div className={ classes.FileListContainer }>
             <h1>Hello World</h1>
         </div>
     );
 };
 FileListContainer.propTypes = {
-    files: PropTypes.array.isRequired
+    files: PropTypes.array.isRequired // TODO be more specific
 };
 
 export default FileListContainer;
--- src/components/UI/FileChooser/FileListItem.js
+++ src/components/UI/FileChooser/FileListItem.js
@@ -1,13 +1,17 @@
+/* eslint-disable */ // TODO delete this
 import React from 'react';
 import PropTypes from 'prop-types';
+import classes from './FileListItem.scss';
 
 const FileListItem = (props) => {
     return (
-        <div />
+        <div className={ classes.FileListItem }>
+
+        </div>
     );
 };
 FileListItem.propTypes = {
-
+    file: PropTypes.object.isRequired // TODO be more specific
 };
 
 export default FileListItem;
