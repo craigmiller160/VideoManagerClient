8ac521dd61ab9f6cd19b839ba84633d129611d3e
Craig Miller <craigmiller160@gmail.com>
2020-01-04 12:13:49 -0500

    Added logic to load the initial files

--- src/components/UI/FileChooser/index.js
+++ src/components/UI/FileChooser/index.js
@@ -1,11 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import { getDirectoriesFromDirectory } from '../../../services/LocalFileApiService';
 
 const FileChooser = (props) => {
     const [state, setState] = useState({
         files: [],
         selectedFile: null
     });
+
+    useEffect(() => {
+        const loadInitialFiles = async () => {
+            const res = await getDirectoriesFromDirectory();
+            setState((prevState) => ({
+                ...prevState,
+                files: res.data
+            }));
+        };
+
+        loadInitialFiles();
+    }, []);
 
     return (
         <div />
