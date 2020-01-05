15d3389ae2f54dc8825145da609ccd986214b6a1
Craig Miller <craigmiller160@gmail.com>
2020-01-05 10:55:51 -0500

    Added icons and otherwise styled the list

--- src/components/UI/FileChooser/FileListItem.js
+++ src/components/UI/FileChooser/FileListItem.js
@@ -2,13 +2,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classes from './FileListItem.scss';
+import directoryIcon from '../../../assets/images/directory.png';
+import fileIcon from '../../../assets/images/file.png';
 
 const FileListItem = (props) => {
     const {
         file
     } = props;
+
+    const icon = file.directory ? directoryIcon : fileIcon;
+    const alt = file.directory ? 'Directory Icon' : 'File Icon';
+
     return (
         <div className={ classes.FileListItem }>
+            <img src={ icon } alt={ alt } />
             <p>{ file.fileName }</p>
         </div>
     );
