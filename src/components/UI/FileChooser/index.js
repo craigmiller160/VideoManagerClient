2b44dda10e26b14ed23d47f1935363924d94f78e
Craig Miller <craigmiller160@gmail.com>
2020-01-05 11:08:23 -0500

    Added the FileChooserContext and some props for the FileChooser itself

--- src/components/UI/FileChooser/index.js
+++ src/components/UI/FileChooser/index.js
@@ -1,20 +1,32 @@
 /* eslint-disable */ // TODO delete this
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { getDirectoriesFromDirectory } from '../../../services/LocalFileApiService';
+import {
+    getDirectoriesFromDirectory,
+    getFilesFromDirectory
+} from '../../../services/LocalFileApiService';
 import FileListContainer from './FileListContainer';
+import FileChooserContext from './FileChooserContext';
 
 const FileChooser = (props) => {
+    const {
+        directoriesOnly
+    } = props;
+
     const [state, setState] = useState({
         fileList: { rootPath: '', files: [] },
         selectedFile: null
     });
 
-    // TODO need some kind of loading indicator
-
     useEffect(() => {
         const loadInitialFiles = async () => {
-            const res = await getDirectoriesFromDirectory();
+            let res;
+            if (directoriesOnly) {
+                res = await getDirectoriesFromDirectory();
+            } else {
+                res = await getFilesFromDirectory();
+            }
+
             setState((prevState) => ({
                 ...prevState,
                 fileList: res.data
@@ -26,12 +38,21 @@ const FileChooser = (props) => {
 
     // TODO need a loading indicator
 
+    const context = {
+        directoriesOnly
+    };
+
     return (
-        <FileListContainer fileList={ state.fileList } />
+        <FileChooserContext.Provider value={ context }>
+            <FileListContainer fileList={ state.fileList } />
+        </FileChooserContext.Provider>
     );
 };
 FileChooser.propTypes = {
-
+    directoriesOnly: PropTypes.bool
+};
+FileChooser.defaultProps = {
+    directoriesOnly: false
 };
 
 export default FileChooser;
