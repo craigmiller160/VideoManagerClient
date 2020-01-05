a02a7a016f139548b8a2244e9dd6e01d67200cc8
Craig Miller <craigmiller160@gmail.com>
2020-01-05 16:09:55 -0500

    Began working on action buttons

--- src/components/UI/FileChooser/FileListItem.js
+++ src/components/UI/FileChooser/FileListItem.js
@@ -1,22 +1,41 @@
 /* eslint-disable */ // TODO delete this
-import React from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
+import { Button } from 'reactstrap';
 import classes from './FileListItem.scss';
 import directoryIcon from '../../../assets/images/directory.png';
 import fileIcon from '../../../assets/images/file.png';
+import FileChooserContext from './FileChooserContext';
 
 const FileListItem = (props) => {
     const {
         file
     } = props;
 
+    const { directoriesOnly } = useContext(FileChooserContext);
+
     const icon = file.directory ? directoryIcon : fileIcon;
     const alt = file.directory ? 'Directory Icon' : 'File Icon';
 
+    let btnColor;
+    let btnLabel;
+    if (directoriesOnly || file.directory) {
+        btnColor = 'info';
+        btnLabel = 'Open';
+    } else {
+        btnColor = 'primary';
+        btnLabel = 'Select';
+    }
+
     return (
         <div className={ classes.FileListItem }>
-            <img src={ icon } alt={ alt } />
-            <p>{ file.fileName }</p>
+            <div className={ classes['label-container'] }>
+                <img src={ icon } alt={ alt } />
+                <p>{ file.fileName }</p>
+            </div>
+            <div className={ classes['btn-container'] }>
+                <Button color={ btnColor }>{ btnLabel }</Button>
+            </div>
         </div>
     );
 };
