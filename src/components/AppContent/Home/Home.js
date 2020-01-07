d6f667c56289877846f87e7afae683a0f13a8256
Craig Miller <craigmiller160@gmail.com>
2020-01-07 17:19:16 -0500

    Validated select

--- src/components/AppContent/Home/Home.js
+++ src/components/AppContent/Home/Home.js
@@ -1,14 +1,20 @@
+/* eslint-disable */ // TODO delete this
 import React from 'react';
 import classes from './Home.scss';
 import FileChooser from '../../UI/FileChooser';
 
-const Home = () => (
-    <div className={ classes.Home }>
-        <div className={ classes.title }>
-            <h3>Welcome to VideoManager</h3>
+const Home = () => {
+
+    const select = (file) => console.log('Selected', file); // TODO delete this
+
+    return (
+        <div className={ classes.Home }>
+            <div className={ classes.title }>
+                <h3>Welcome to VideoManager</h3>
+            </div>
+            <FileChooser directoriesOnly selectFile={ select } />
         </div>
-        <FileChooser directoriesOnly />
-    </div>
-);
+    )
+};
 
 export default Home;
