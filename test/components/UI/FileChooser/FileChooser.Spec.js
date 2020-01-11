596cdf7cff1bf605b9661ef7d3eb410d474e9b93
Craig Miller <craigmiller160@gmail.com>
2020-01-11 16:31:56 -0500

    Stubbed out unit tests for the FileChooser parent component

--- test/components/UI/FileChooser/FileChooser.Spec.js
+++ test/components/UI/FileChooser/FileChooser.Spec.js
@@ -1,5 +1,25 @@
 describe('FileChooser', () => {
-    it('write tests', () => {
-        throw new Error();
+    describe('rendering', () => {
+        it('renders while loading', () => {
+            throw new Error();
+        });
+
+        it('renders after loading completed', () => {
+            throw new Error();
+        });
+
+        it('loads only directories', () => {
+            throw new Error();
+        });
+    });
+
+    describe('actions', () => {
+        it('openDirectory', () => {
+            throw new Error();
+        });
+
+        it('selectFile', () => {
+            throw new Error();
+        });
     });
 });
