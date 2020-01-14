89ba38ee0ce562d42ce09cab741c4557e357203a
Craig Miller <craigmiller160@gmail.com>
2020-01-13 19:06:45 -0500

    Integrated settings loading behavior and stubbed out the form component

---
+++ src/components/AppContent/Settings/SettingsForm.js
@@ -0,0 +1,12 @@
+/* eslint-disable */ // TODO delete this
+import React from 'react';
+
+const SettingsForm = () => {
+    return (
+        <div>
+            <h1>Settings Form</h1>
+        </div>
+    );
+};
+
+export default SettingsForm;
--- src/components/AppContent/Settings/index.js
+++ src/components/AppContent/Settings/index.js
@@ -1,9 +1,29 @@
 /* eslint-disable */ // TODO delete this
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import classes from './Settings.scss';
 import FlexRow from '../../UI/Grid/FlexRow';
+import { getSettings } from '../../../services/SettingsApiService';
+import Spinner from '../../UI/Spinner/Spinner';
+import SettingsForm from './SettingsForm';
 
 const Settings = () => {
+    const [state, setState] = useState({
+        initialValues: {},
+        loading: true
+    });
+
+    useEffect(() => {
+        const loadData = async () => {
+            const res = await getSettings();
+            setState((prevState) => ({
+                ...prevState,
+                initialValues: res.data,
+                loading: false
+            }));
+        };
+        loadData();
+    }, []);
+
     return (
         <div className={ classes.Settings }>
             <FlexRow>
@@ -11,6 +31,14 @@ const Settings = () => {
                     <h3>Settings</h3>
                 </div>
             </FlexRow>
+            {
+                state.loading &&
+                    <Spinner />
+            }
+            {
+                !state.loading &&
+                    <SettingsForm />
+            }
         </div>
     );
 };
