04dfd4762fd8ef7f4f6ac8cb0de8c583509c5930
Craig Miller <craigmiller160@gmail.com>
2020-01-20 15:01:24 -0500

    Stubbed out unit tests

--- test/store/settings/settings.actions.Spec.js
+++ test/store/settings/settings.actions.Spec.js
@@ -1,5 +1,27 @@
 describe('settings.actions', () => {
-    it('write tests', () => {
+    it('setLoading', () => {
         throw new Error();
+    });
+
+    describe('thunk actions', () => {
+        describe('loadSettings', () => {
+            it('loads settings', () => {
+                throw new Error();
+            });
+
+            it('has error', () => {
+                throw new Error();
+            });
+        });
+
+        describe('saveSettings', () => {
+            it('saves settings', () => {
+                throw new Error();
+            });
+
+            it('has error', () => {
+                throw new Error();
+            });
+        });
     });
 });
