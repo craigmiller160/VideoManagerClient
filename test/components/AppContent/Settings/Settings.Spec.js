26467f591feb08e406297f0e901690e4ac3d2480
Craig Miller <craigmiller160@gmail.com>
2020-01-25 14:06:07 -0500

    Stubbed out tests for Settings page

--- test/components/AppContent/Settings/Settings.Spec.js
+++ test/components/AppContent/Settings/Settings.Spec.js
@@ -1,5 +1,52 @@
+import React from 'react';
+import mountTestComponent from '../../../exclude/testUtil/mountTestComponent';
+import Settings from 'components/AppContent/Settings';
+
+jest.mock('components/UI/FileChooser', () => {
+    const FileChooser = (props) => <div { ...props } />;
+    return FileChooser;
+});
+
+const defaultStoreState = {
+    settings: {
+        loading: false
+    }
+};
+
+const doMount = mountTestComponent(Settings, {
+    defaultStoreState
+});
+
 describe('Settings', () => {
-    it('write tests', () => {
-        throw new Error();
+    describe('rendering', () => {
+        it('renders while loading', () => {
+            throw new Error();
+        });
+
+        it('renders with form, no file chooser', () => {
+            throw new Error();
+        });
+
+        it('renders with form and file chooser', () => {
+            throw new Error();
+        });
+    });
+
+    describe('actions', () => {
+        it('dispatches loadSettings on mount', () => {
+            throw new Error();
+        });
+
+        it('calls editRootDir', () => {
+            throw new Error();
+        });
+
+        it('calls selectDir', () => {
+            throw new Error();
+        });
+
+        it('submits form', () => {
+            throw new Error();
+        });
     });
 });
