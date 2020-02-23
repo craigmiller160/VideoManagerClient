e394218adeeecbaf2c61ddbe90f5a8d6933c53fd
Craig Miller <craigmiller160@gmail.com>
2020-02-23 10:45:23 -0500

    Updated unit tests for ToolTip

--- test/components/UI/ToolTip/ToolTip.Spec.js
+++ test/components/UI/ToolTip/ToolTip.Spec.js
@@ -1,5 +1,18 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import ToolTip from 'components/UI/ToolTip';
+
 describe('ToolTip', () => {
-    it('write tests', () => {
-        throw new Error();
+    describe('rendering', () => {
+        it('renders correctly', () => {
+            const component = shallow(
+                <ToolTip text="My Text">
+                    <p>Hello World</p>
+                </ToolTip>
+            );
+            expect(component.find('div.ToolTip')).toHaveLength(1);
+            expect(component.find('p').text()).toEqual('Hello World');
+            expect(component.find('span.ToolTipText').text()).toEqual('My Text');
+        });
     });
 });
