661a15706cf30b8fbfaacf6f9614580e55259b29
Craig Miller <craigmiller160@gmail.com>
2020-03-08 10:35:16 -0400

    Added the video.js css

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import '!style-loader!css-loader!bootstrap/dist/css/bootstrap.min.css'; // eslint-disable-line import/no-webpack-loader-syntax
+import '!style-loader!css-loader!video.js/dist/video-js.min.css'; // eslint-disable-line import/no-webpack-loader-syntax
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
