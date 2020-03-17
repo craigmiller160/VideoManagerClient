bf12ee78bdfc8a07b8ec72ac52e5a9306b63f2a1
Craig Miller <craigmiller160@gmail.com>
2020-03-17 17:10:22 -0400

    Migrated VideoPlayer.Spec

--- test/components/AppContent/VideoPlayer/VideoPlayer.Spec.js
+++ test/components/AppContent/VideoPlayer/VideoPlayer.Spec.js
@@ -1,19 +1,20 @@
 import VideoPlayer from 'components/AppContent/VideoPlayer/VideoPlayer';
-import mountTestComponent from '../../../exclude/testUtil/mountTestComponent';
+import enzymeCreator from 'react-enzyme-utils';
 
 const defaultProps = {
     fileId: 1,
     videoToken: 'token'
 };
 
-const doMount = mountTestComponent(VideoPlayer, {
-    defaultProps
+const mounter = enzymeCreator({
+    component: VideoPlayer,
+    props: defaultProps
 });
 
 describe('VideoPlayer', () => {
     describe('rendering', () => {
         it('renders correctly', () => {
-            const { component } = doMount();
+            const { component } = mounter();
             expect(component.find('video')).toHaveLength(1);
             expect(component.find('source')).toHaveLength(1);
             expect(component.find('source').props()).toEqual({
