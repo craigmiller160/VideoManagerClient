390dd9911cf7b1dd032be27ebe375d79b8e95b44
Craig Miller <craigmiller160@gmail.com>
2020-12-25 16:27:03 -0500

    Upgraded config lib and imported polyfills

--- src/index.js
+++ src/index.js
@@ -16,6 +16,7 @@
  *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
+import '@craigmiller160/react-web-config/polyfills';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
