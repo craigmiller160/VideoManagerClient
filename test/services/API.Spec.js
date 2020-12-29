ce3a06b458e129fb719ea3fc0a53fb744e0c7e56
Craig Miller <craigmiller160@gmail.com>
2020-12-29 14:57:08 -0500

    Stubbed out new tests

--- test/services/API.Spec.js
+++ test/services/API.Spec.js
@@ -21,90 +21,32 @@ import { CSRF_TOKEN_KEY } from 'utils/securityConstants';
 import MockAdapter from 'axios-mock-adapter';
 import { mockCsrfToken } from '../exclude/mock/mockApiConfig/authApi';
 
-jest.mock('store/store', () => {
-    let csrfToken = '';
-    let actions = [];
-
-    return {
-        getState() {
-            return {
-                auth: {
-                    csrfToken
-                }
-            }
-        },
-        setCsrfToken(token) {
-            csrfToken = token
-        },
-        clearCsrfToken() {
-            csrfToken = '';
-        },
-        dispatch(action) {
-            actions.push(action);
-        },
-        getActions() {
-            return actions;
-        },
-        clearActions() {
-            actions = [];
-        }
-    }
-});
-
-import store from 'store/store'; // eslint-disable-line import/first
-
-const apiGetSpy = jest.spyOn(API, 'get');
-const apiRequestSpy = jest.spyOn(API, 'request');
-
-const contentType = 'Content-Type';
-const applicationJson = 'application/json';
-
 const mockApi = new MockAdapter(API);
 
 describe('API', () => {
     beforeEach(() => {
-        store.clearCsrfToken();
-        store.clearActions();
         mockApi.reset();
-        apiRequestSpy.mockClear();
-        apiGetSpy.mockClear();
     });
 
     describe('addCsrfTokenInterceptor', () => {
-        const config = {
-            method: 'post',
-            headers: {
-                [contentType]: applicationJson
-            }
-        };
-
-        it('adds token if exists', () => {
-            store.setCsrfToken(mockCsrfToken);
-            const result = addCsrfTokenInterceptor(config);
-            expect(result).toEqual({
-                ...config,
-                headers: {
-                    ...config.headers,
-                    [CSRF_TOKEN_KEY]: mockCsrfToken
-                }
-            });
+        it('does nothing for GET', () => {
+            throw new Error();
         });
 
-        it('does not add token if not exists', () => {
-            const result = addCsrfTokenInterceptor(config);
-            expect(result).toEqual(config);
+        it('adds CSRF token for POST', () => {
+            throw new Error();
         });
 
-        it('does not add token if method is get', () => {
-            store.setCsrfToken(mockCsrfToken);
-            const result = addCsrfTokenInterceptor({ ...config, method: 'get' });
-            expect(result).toEqual({
-                ...config,
-                method: 'get',
-                headers: {
-                    ...config.headers
-                }
-            });
+        it('adds CSRF token for PUT', () => {
+            throw new Error();
+        });
+
+        it('adds CSRF token for DELETE', () => {
+            throw new Error();
+        });
+
+        it('has error while getting CSRF token', () => {
+            throw new Error();
         });
     });
 });
