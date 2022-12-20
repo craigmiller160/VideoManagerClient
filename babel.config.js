273098e2c092ffbbce026e176b0548a610e4bda9
Craig Miller <craigmiller160@gmail.com>
2022-12-20 16:39:33 -0500

    Another attempt to get it working

--- babel.config.js
+++ babel.config.js
@@ -1,2 +1,6 @@
 // Currently only used for ESLint and ignored by legacy build system
-module.exports = require('@craigmiller160/babel-config');
+const baseConfig = require('@craigmiller160/babel-config');
+const reactConfig = require('@craigmiller160/babel-config-react');
+const configMerge = require('@craigmiller160/config-merge');
+
+module.exports = configMerge(baseConfig, reactConfig);
