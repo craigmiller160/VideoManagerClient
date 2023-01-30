27f651c4e8c86b31af25361c689a292d0feff010
Craig Miller <craigmiller160@gmail.com>
2023-01-30 16:47:13 -0500

    Updated proxy settings for dev server

--- vite.config.js
+++ vite.config.js
@@ -4,37 +4,35 @@ import fs from 'fs';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    root: path.join(process.cwd(), 'src'),
-    base: '/expense-tracker/',
-    publicDir: path.join(process.cwd(), 'public'),
-    server: {
-        port: 3000,
-        host: true,
-        https: {
-            cert: fs.readFileSync(
-                path.join(
-                    process.cwd(),
-                    'dev',
-                    'certs',
-                    'localhost.cert.pem'
-                ),
-                'utf8'
-            ),
-            key: fs.readFileSync(
-                path.join(
-                    process.cwd(),
-                    'dev',
-                    'certs',
-                    'localhost.key.pem'
-                ),
-                'utf8'
-            )
-        }
-    },
-    plugins: [react()],
-    build: {
-        target: 'esnext',
-        outDir: path.join(process.cwd(), 'build'),
-        emptyOutDir: true
-    }
+	root: path.join(process.cwd(), 'src'),
+	base: '/video-manager/',
+	publicDir: path.join(process.cwd(), 'public'),
+	server: {
+		port: 3000,
+		host: true,
+		https: {
+			cert: fs.readFileSync(
+				path.join(process.cwd(), 'dev', 'certs', 'localhost.cert.pem'),
+				'utf8'
+			),
+			key: fs.readFileSync(
+				path.join(process.cwd(), 'dev', 'certs', 'localhost.key.pem'),
+				'utf8'
+			)
+		},
+		proxy: {
+			'/video-manager/api': {
+				target: 'https://localhost:8443',
+				secure: false,
+				rewrite: (path) => path.replace(/^\/video-manager\/api/, ''),
+				logLevel: 'debug'
+			}
+		}
+	},
+	plugins: [react()],
+	build: {
+		target: 'esnext',
+		outDir: path.join(process.cwd(), 'build'),
+		emptyOutDir: true
+	}
 });
