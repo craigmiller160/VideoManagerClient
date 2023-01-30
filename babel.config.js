bf1432c199425ae12c689aa3398664a60714215a
Craig Miller <craigmiller160@gmail.com>
2023-01-30 16:07:33 -0500

    Babel enhancements

--- babel.config.js
+++ babel.config.js
@@ -1,1 +1,5 @@
-module.exports = require('@craigmiller160/babel-config');
+const babelConfig = require('@craigmiller160/babel-config');
+const reactConfig = require('@craigmiller160/babel-config-react');
+const configMerge = require('@craigmiller160/config-merge');
+
+module.exports = configMerge(babelConfig, reactConfig);
