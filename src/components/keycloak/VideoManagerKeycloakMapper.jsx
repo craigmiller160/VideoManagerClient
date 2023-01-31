78bf822784a2b143091282bbce80fb29a0e03c48
Craig Miller <craigmiller160@gmail.com>
2023-01-31 14:50:57 -0500

    Integrating the keycloak token into redux auth store

--- src/components/keycloak/VideoManagerKeycloakMapper.jsx
+++ src/components/keycloak/VideoManagerKeycloakMapper.jsx
@@ -1,6 +1,25 @@
 import PropTypes from 'prop-types';
+import {useContext, useEffect} from "react";
+import {KeycloakAuthContext} from "@craigmiller160/react-keycloak";
+import {useDispatch} from "react-redux";
+import {setIsAuth, setUserDetails} from "../../store/auth/auth.actions";
+
+const mapUserDetails = (token) => ({
+	firstName: '',
+	lastName: '',
+	username: '',
+	roles: []
+});
 
 export const VideoManagerKeycloakMapper = (props) => {
+	const keycloakAuth = useContext(KeycloakAuthContext);
+	const dispatch = useDispatch();
+	useEffect(() => {
+		dispatch(setIsAuth(keycloakAuth.isAuthorized));
+		if (keycloakAuth.isAuthorized) {
+			dispatch(setUserDetails(mapUserDetails(keycloakAuth.token)));
+		}
+	}, [dispatch, keycloakAuth]);
 	return props.children;
 };
 VideoManagerKeycloakMapper.propTypes = {
