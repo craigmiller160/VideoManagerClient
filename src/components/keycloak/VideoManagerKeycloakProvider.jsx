cc579a979d1f2dcc3ac8ceca43ff83ff9b306e02
Craig Miller <craigmiller160@gmail.com>
2023-02-06 08:51:23 -0500

    Refactoring use of auth provider

--- src/components/keycloak/VideoManagerKeycloakProvider.jsx
+++ src/components/keycloak/VideoManagerKeycloakProvider.jsx
@@ -3,8 +3,6 @@ import { BEARER_TOKEN_KEY } from '@craigmiller160/ajax-api';
 import PropTypes from 'prop-types';
 import { VideoManagerKeycloakMapper } from './VideoManagerKeycloakMapper';
 
-const ACCESS_TOKEN_EXP_SECS = 300;
-
 const getRealm = () => {
 	if (process.env.NODE_ENV !== 'test') {
 		return import.meta.env.VITE_KEYCLOAK_REALM;
@@ -14,11 +12,9 @@ const getRealm = () => {
 
 export const VideoManagerKeycloakProvider = (props) => (
 	<KeycloakAuthProvider
-		accessTokenExpirationSecs={ACCESS_TOKEN_EXP_SECS}
 		realm={getRealm()}
-		authServerUrl="https://auth-craigmiller160.ddns.net/"
 		clientId="video-manager-client"
-		bearerTokenLocalStorageKey={BEARER_TOKEN_KEY}
+		localStorageKey={BEARER_TOKEN_KEY}
 	>
 		<VideoManagerKeycloakMapper>
 			{props.children}
