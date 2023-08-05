565105c7e7ea256a39eee38813b9c3f79c94673f
Craig Miller <craigmiller160@gmail.com>
2023-08-05 11:59:11 -0400

    Tweak to import script

--- deploy/terraform/import.sh
+++ deploy/terraform/import.sh
@@ -15,3 +15,4 @@ function plan {
 import "keycloak_openid_client.video_manager_client_dev" "apps-dev/19817468-5a8b-4044-bdd2-93672a46f1f7"
 import "keycloak_openid_client.video_manager_client_prod" "apps-prod/87df63cf-58ae-4aa7-b2ee-5e47cb2e05f0"
 
+plan
